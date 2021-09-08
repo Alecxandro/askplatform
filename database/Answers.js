84a115d837071bb4c4ec52fa7571ec2c0c97cf40
Alex <alecxandroch@gmail.com>
2021-09-08 14:16:02 -0300

    version 1.0 done

--- database/Answers.js
+++ database/Answers.js
@@ -1,9 +1,21 @@
 const Sequelize = require('sequelize');
 const connection = require('./database');
 
-const Answers = new connection.define('answers',{
+const Answers = connection.define('answers',{
     description: {
         type: Sequelize.TEXT,
         allowNull: false
+    },
+    questionId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
     }
 })
+
+Answers.sync({force:false}).then(()=>{
+    console.log('Table Answers created successfully!')
+}).catch((err)=>{
+    console.log(err);
+})
+
+module.exports = Answers;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const port = 8080;
+
+//models
+
 const connection = require("./database/database");
 const Questions = require("./database/Questions");
-const { response } = require("express");
+const Answers = require("./database/Answers");
 
-//database configuration
+//database configuration/connection
+
 connection
   .authenticate()
   .then(() => {
@@ -14,14 +19,19 @@ connection
   .catch((msgerr) => {
     console.log(msgerr);
   });
+
 //
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 //
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Routes
+
 app.get("/", (req, res) => {
   Questions.findAll({ raw: true, order: [["id", "DESC"]] }).then(
     (questions) => {
@@ -40,18 +50,42 @@ app.get("/ask", (req, res) => {
 app.get("/question/:id", (req, res) => {
   const id = req.params.id;
 
+  //
   Questions.findOne({
     where: { id: id },
   }).then((question) => {
     if (question != undefined) {
-      res.render("question", {
-        qTitle: question.title,
-        qDescription: question.description,
+      Answers.findAll({
+        where: { questionId: question.id },
+      }).then((answr) => {
+        res.render("question", {
+          question: question,
+          answrs: answr,
+        });
       });
     } else {
       res.redirect("/");
     }
   });
+  //
+});
+
+app.post("/sendanswer", (req, res) => {
+  const answerDescription = req.body.answerDescription;
+  const questionId = req.body.questionId;
+
+  if (answerDescription) {
+    Answers.create({
+      description: answerDescription,
+      questionId: questionId,
+    })
+      .then(() => {
+        res.redirect(`/question/${questionId}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
 app.post("/savequestion", (req, res) => {
@@ -73,6 +107,6 @@ app.post("/savequestion", (req, res) => {
   }
 });
 
-app.listen(8080, () => {
-  console.log("listening on port 8080...");
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
 });
